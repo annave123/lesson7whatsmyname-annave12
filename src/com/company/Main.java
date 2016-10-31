be0b1e36afac654886ac56db418bf1beafec57fe
annave123 <tony.nave@aol.com>
2016-10-30 20:26:10 -1000

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner Names = new Scanner(System.in);
+        System.out.println("Please enter your first name");
+        String firstName = Names.next();
+        System.out.println("Please enter your second name");
+        String secondName = Names.next();
+        System.out.println("your full name is "+firstName+ " "+ secondName);
+
     }
-}
+ }
